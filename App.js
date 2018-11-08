123edd8f2bb5b83b97caca70ec27b3fc72d5bd68
Delaney Eichorn <delaneyeichorn@MacBook-Pro-5.local>
2018-11-08 10:29:55 -0500

    Add rough donate food screen

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import { createStackNavigator } from 'react-navigation';
 import { HomeScreen } from './src/screens/Home';
 import { LogInScreen } from './src/screens/LogIn';
 import { SignUpScreen } from './src/screens/SignUp';
+import { DashboardScreen } from './src/screens/DonateFood';
 
 const RootStack = createStackNavigator({
+  DonateFood: {
+    screen: DonateFood
+  },
   Home: {
     screen: HomeScreen
   },
---
+++ src/screens/DonateFood.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  StyleSheet, SafeAreaView, View, Text, Button, Dimensions, TextInput,
+  Picker
+} from 'react-native';
+import { LongButton } from '../components/LongButton';
+import { Title } from '../components/Title';
+
+export class DashboardScreen extends React.Component {
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={{ height: Dimensions.get('window').height, backgroundColor: 'white' }}>
+        <Text style={styles.header}>Donate Now</Text>
+        <View>
+          <View style={{width: 700, height: 7, backgroundColor: 'powderblue'}} />
+        </View>
+        <Text style={styles.body}>Food Type</Text>
+        <Text style={styles.body}>Food Description</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="E.g. Fried Chicken, Goldfish"
+        />
+        <Text style={styles.body}>Estimated Weight</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Lbs."
+        />
+        <Text style={styles.body}>Estimated Expiration Date</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="mm/dd/yy"
+        />
+        <Button
+            style={styles.button}
+            title="+ Add Item"
+          />
+      </View>
+     );
+   }
+}
+
+
+const styles = StyleSheet.create({
+  header: {
+    padding: 15,
+    paddingTop: 20,
+    fontSize: 30,
+  },
+  body: {
+    padding: 17,
+    paddingTop: 10,
+    fontSize: 20,
+  },
+  input: {
+    height: 20,
+    fontSize: 17,
+    padding: 17,
+    paddingTop: 0.1,
+    color: 'black',
+    height: 35,
+  },
+  button: {
+    fontSize: 20,
+    paddingTop: 15,
+  }
+});
