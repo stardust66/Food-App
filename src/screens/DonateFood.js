8696ed58c016293ca120ba3c73eaec307da4991f
Jason Chen <kbtpodifo@gmail.com>
2018-11-12 00:46:43 -0500

    Make progress on DonateFood screen

--- src/screens/DonateFood.js
+++ src/screens/DonateFood.js
@@ -1,68 +1,103 @@
 import React from 'react';
 import {
   StyleSheet, SafeAreaView, View, Text, Button, Dimensions, TextInput,
-  TouchableOpacity
+  TouchableOpacity, ScrollView, TouchableWithoutFeedback, Keyboard
 } from 'react-native';
 import { LongButton } from '../components/LongButton';
 import { Title } from '../components/Title';
+import { DetailInput } from '../components/DetailInput';
 import QuickPicker from 'quick-picker';
+
+const DEFAULT_FOOD = {
+  foodType: 'Packaged',
+  foodDescription: '',
+  estimatedWeight: '',
+  estimatedExpirationDate: ''
+};
 
 export class DonateFoodScreen extends React.Component {
 
-  state = {
-    foodType: 'Packaged',
-    foodDescription: '',
-    estimatedWeight: '',
-    estimatedExpirationDate: ''
+  static navigationOptions = {
+    title: 'Donate'
   }
 
-  showFoodTypePicker = () => {
+  state = {
+    foods: [Object.assign({}, DEFAULT_FOOD)]
+  }
+
+  showFoodTypePicker = (index) => {
+    console.log(index);
     QuickPicker.open({
       items: ['Packaged', 'Prepared'],
-      selectedValue: this.state.foodType,
-      onValueChange: (value) => this.setState({foodType: value})
+      selectedValue: this.state.foods[index].foodType,
+      onValueChange: (value) => this.setState((prevState) => {
+        foods: [...prevState.foods.slice(0, index),
+                Object.assign(prevState.foods[index], {foodType: value}),
+                ...prevState.foods.slice(index+1)]
+      })
     });
+  }
+
+  addItem = () => {
+    this.setState((prevState) => ({
+      foods: [...prevState.foods, Object.assign({}, DEFAULT_FOOD)]
+    }));
+  }
+
+  updateFood = (index, updateObject) => {
+    this.setState((prevsState) => ({
+      foods: [...prevState.foods.slice(0, index),
+              Object.assign(prevState.foods[index], updateObject),
+              ...prevState.foods.slice(index+1)]
+    }));
+    console.log(this.state);
   }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View style={{ height: Dimensions.get('window').height, backgroundColor: 'white' }}>
-        <Text style={styles.header}>Donate Now</Text>
-        <View>
+      <TouchableWithoutFeedback style={{flex: 1}} onPress={Keyboard.dismiss} accessible={false}>
+        <ScrollView style={{ height: Dimensions.get('window').height, backgroundColor: 'white' }}>
+          <Text style={styles.header}>Items</Text>
           <View style={{width: 700, height: 7, backgroundColor: 'powderblue'}} />
-        </View>
-        <Text style={styles.body}>Food Type</Text>
-        <TouchableOpacity
-          onPress={this.showFoodTypePicker}
-        >
-          <Text style={styles.input}>{this.state.foodType}</Text>
-        </TouchableOpacity>
-        <QuickPicker />
-        <Text style={styles.body}>Food Description</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="E.g. Fried Chicken, Goldfish"
-        />
-        <Text style={styles.body}>Estimated Weight</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Lbs."
-        />
-        <Text style={styles.body}>Estimated Expiration Date</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="mm/dd/yy"
-        />
-        <Button
-          style={styles.button}
-          title="+ Add Item"
-        />
-      </View>
+          <QuickPicker />
+          {this.state.foods.map((food, index) => (
+            <React.Fragment key={index}>
+              <Text style={styles.body}>Food Type</Text>
+              <TouchableOpacity
+                onPress={this.showFoodTypePicker.bind(this, index)}
+              >
+                <Text style={styles.input}>{food.foodType}</Text>
+              </TouchableOpacity>
+              <Text style={styles.body}>Food Description</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="E.g. Fried Chicken, Goldfish"
+                value={food.foodDescription}
+              />
+              <Text style={styles.body}>Estimated Weight</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Lbs."
+              />
+              <Text style={styles.body}>Estimated Expiration Date</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="mm/dd/yy"
+              />
+              <View style={{width: 700, height: 7, backgroundColor: 'powderblue'}} />
+            </React.Fragment>
+          ))}
+          <Button
+            style={styles.button}
+            title="+ Add Item"
+            onPress={this.addItem}
+          />
+        </ScrollView>
+      </TouchableWithoutFeedback>
      );
    }
 }
-
 
 const styles = StyleSheet.create({
   header: {
@@ -86,5 +121,6 @@ const styles = StyleSheet.create({
   button: {
     fontSize: 20,
     paddingTop: 15,
+    paddingBottom: 15
   }
 });
